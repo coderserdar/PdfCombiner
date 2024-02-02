d27d7cd47a84e2e587efd69dacce786e6e62b11c
Sudhanshu Shekhar <sudhanshu.shekhar@Geminisolutions.com>
2024-02-02 01:41:16 -0600

    PDF Combiner v4

--- SourceCode/PdfCombiner/PdfCombiner/Properties/AssemblyInfo.cs
+++ SourceCode/PdfCombiner/PdfCombiner/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Pdf Combiner")]
-[assembly: AssemblyDescription("Prepared by Sudhanshu Shekhar")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Sudhanshu")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Pdf Combiner")]
 [assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
